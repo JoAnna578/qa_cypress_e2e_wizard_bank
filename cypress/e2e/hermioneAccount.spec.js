848676c3b0b0a2b87c6607b6118446538bb67cb3
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-25 13:44:15 +0200

    Improve transaction and balance assertions in Hermione account test

--- cypress/e2e/hermioneAccount.spec.js
+++ cypress/e2e/hermioneAccount.spec.js
@@ -1,66 +1,52 @@
-/// <reference types="cypress" />
+/// <reference types='cypress' />
 
 describe('Bank app', () => {
   before(() => {
-    cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login');
+    cy.visit('/');
   });
 
-  it('should provide the ability to work with Hermione\'s bank account', () => {
+  it("should provide the ability to work with Hermione's bank account", () => {
     // Kliknij Customer Login
     cy.contains('Customer Login').click();
 
     // Wybierz Hermione Granger i zaloguj się
-    cy.get('#userSelect').select('Hermione Granger');
-    cy.get('button[type="submit"]').click();
+    cy.get('select').select('Hermione Granger');
+    cy.contains('Login').click();
 
     // Sprawdź numer konta, saldo i walutę
     cy.get('.ng-binding').contains('1001').should('be.visible');
-    cy.get('.ng-binding').contains('0').should('be.visible');
-    cy.get('.ng-binding').contains('Rupee').should('be.visible');
+    cy.get('.ng-binding').eq(1).should('contain', '0'); // precyzyjnie wybiera saldo
+    cy.get('.ng-binding').contains('Dollar').should('be.visible');
 
-    // Kliknij Deposit
+    // Wpłata 100
     cy.contains('Deposit').click();
+    cy.get('input[placeholder="amount"]').type('100');
+    cy.contains('Deposit').click();
+    cy.contains('Deposit Successful').should('be.visible');
+    cy.get('.ng-binding').eq(1).should('contain', '100');
 
-    // Wpłać 100
-    cy.get('input[type="number"]').type('100');
-    cy.get('form button[type="submit"]').click();
-
-    // Sprawdź komunikat sukcesu i saldo
-    cy.get('.error').should('contain', 'Deposit Successful');
-    cy.get('.ng-binding').contains('100').should('be.visible');
-
-    // Kliknij Withdrawl
+    // Wypłata 50
     cy.contains('Withdrawl').click();
+    cy.get('input[placeholder="amount"]').type('50');
+    cy.contains('Withdraw').click();
+    cy.contains('Transaction successful').should('be.visible');
+    cy.get('.ng-binding').eq(1).should('contain', '50');
 
-    // Wypłać 50
-    cy.get('input[type="number"]').type('50');
-    cy.get('form button[type="submit"]').click();
-
-    // Sprawdź komunikat sukcesu i saldo
-    cy.get('.error').should('contain', 'Transaction successful');
-    cy.get('.ng-binding').contains('50').should('be.visible');
-
-    // Kliknij Transactions i sprawdź obie transakcje
+    // Sprawdź transakcje
     cy.contains('Transactions').click();
-    cy.get('table tbody tr').should('have.length', 2);
-    cy.get('table tbody tr').first().should('contain', 'Deposit');
-    cy.get('table tbody tr').last().should('contain', 'Withdrawal');
+    cy.get('table tbody tr').first().should('contain', 'Deposit').and('contain', '100');
+    cy.get('table tbody tr').last().should('contain', 'Withdrawal').and('contain', '50');
 
-    // Kliknij Back
+    // Cofnij
     cy.contains('Back').click();
 
-    // Zmień numer konta
+    // Zmień konto
     cy.get('#accountSelect').select('1002');
     cy.contains('Transactions').click();
+    cy.get('table tbody tr').should('not.exist');
 
-    // Sprawdź, że nie ma transakcji
-    cy.get('table tbody tr').should('have.length', 0);
-
-    // Kliknij Back i Logout
-    cy.contains('Back').click();
+    // Wyloguj
     cy.contains('Logout').click();
-
-    // Sprawdź, że użytkownik jest wylogowany
     cy.contains('Your Name :').should('be.visible');
   });
 });
